2d2f43e25ea288cca967c2330678288afd702f5e
kickinbahk@gmail.com <kickinbahk@gmail.com>
2016-11-21 07:35:26 -0800

    Initial Commit

---
+++ Sorting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sorting
+//
+//  Created by Josiah Mory on 11/21/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
